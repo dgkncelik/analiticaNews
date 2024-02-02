6dcb17bae56c0abb10ecfb54c70e7d6c85a9b3e0
Doğukan Çelik <dogukan.celik@btsgrp.com>
2024-02-02 17:15:36 +0300

    Add restApi

--- api.py
+++ api.py
@@ -3,12 +3,19 @@ from fastapi import FastAPI
 import uvicorn
 import sqlite3
 import sql_functions
+import elasticsearch
+import requests
+import json
 
 app = FastAPI()
 
 API_DB_NAME = "scrape_database.db"
 API_ELASTIC_URL = "http://54.85.90.67:9200"
 conn = sqlite3.connect(API_DB_NAME, check_same_thread=False)
+es_conn = elasticsearch.Elasticsearch(
+    API_ELASTIC_URL,  # Elasticsearch endpoint
+)
+
 
 @app.get("/")
 def read_root():
@@ -37,6 +44,33 @@ def get_all_news():
         news.append(n)
 
     return {"haberler": news}
+
+# Swagger doc
+# Desription: sdufhsdkf
+@app.get("/populerhaberler")
+def get_popular_news():
+    es_query = {
+        "query": {
+            "match_all": {},
+
+        },
+        "aggs": {
+            "common_keyword": {
+                "terms": {
+                    "field": "title.keyword"
+                }
+            }
+        }
+    }
+    populer_haberler = []
+    d = json.dumps(es_query)
+    response = requests.post("http://54.85.90.67:9200/haberler/_search", data=d,
+                             headers={"Content-Type": "application/json"})
+    content = json.loads(response.content)
+    aggs = content["aggregations"]["common_keyword"]["buckets"]
+    for agg in aggs:
+        populer_haberler.append(agg)
+    return populer_haberler
 
 
 if __name__ == "__main__":
